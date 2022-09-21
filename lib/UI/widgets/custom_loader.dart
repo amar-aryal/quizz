15c109b3c19690d963afe1aacf0e3efca51ed293
Amar Aryal <amararyal878@gmail.com>
2022-09-21 22:42:20 +0545

    flip and rotation animation animation + arc over loader circle

--- lib/UI/widgets/custom_loader.dart
+++ lib/UI/widgets/custom_loader.dart
@@ -18,7 +18,7 @@ class _CustomLoaderState extends State<CustomLoader>
   void initState() {
     super.initState();
     _controller =
-        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+        AnimationController(vsync: this, duration: const Duration(seconds: 2));
 
     _animation = Tween(end: 1.0, begin: 0.0).animate(_controller)
       ..addListener(() {
@@ -43,11 +43,21 @@ class _CustomLoaderState extends State<CustomLoader>
 
   @override
   Widget build(BuildContext context) {
-    return Transform(
+    //* for flip animation
+    // return Transform(
+    //   alignment: FractionalOffset.center,
+    //   transform: Matrix4.identity()
+    //     ..setEntry(3, 2, 0.0015)
+    //     ..rotateY(pi * _animation.value),
+    //   child: CustomPaint(
+    //     painter: ShapePainter(),
+    //     child: Container(),
+    //   ),
+    // );
+    //* for rotating animation
+    return RotationTransition(
       alignment: FractionalOffset.center,
-      transform: Matrix4.identity()
-        ..setEntry(3, 2, 0.0015)
-        ..rotateY(pi * _animation.value),
+      turns: _controller,
       child: CustomPaint(
         painter: ShapePainter(),
         child: Container(),
@@ -62,13 +72,25 @@ class ShapePainter extends CustomPainter {
     var paint = Paint()
       ..color = Colors.green
       ..strokeWidth = 5
-      // ..style = PaintingStyle.stroke
       ..strokeCap = StrokeCap.round;
+
+    final arcPaint = Paint()
+      ..color = Colors.green
+      ..strokeWidth = 5
+      ..style = PaintingStyle.stroke;
 
     Offset centerPoint = Offset(size.width / 2, size.height / 2);
 
     canvas.drawCircle(centerPoint, 50, paint);
+
+    canvas.drawArc(Rect.fromCircle(center: centerPoint, radius: 70), pi,
+        pi * 0.5, false, arcPaint);
+
+    canvas.drawArc(Rect.fromCircle(center: centerPoint, radius: 70), pi * 2,
+        pi / 2, false, arcPaint);
   }
+
+  double doubleToAngle(double angle) => angle * pi / 180.0;
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) {
